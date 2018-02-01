fec8a9baa5670e58a74fbdcf41160a917fb8f373
Theodor Dickens <d.theodor@olonaut.de>
2018-02-01 16:57:33 +0100

    Initial shitfit

---
+++ Assets/Scripts/WorldGen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGen : MonoBehaviour {
+
+	public GameObject grass_prefab;
+	// Use this for initialization
+	void Start () {
+		
+		MonoBehaviour.Instantiate(grass_prefab,new Vector3(1,1,1),new Quaternion(0,0,0,0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
+
