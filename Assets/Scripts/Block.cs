4260996ec21ce64ddd3cbdbcbbde771990399584
Theodor Dickens <d.theodor@olonaut.de>
2018-02-02 16:49:33 +0100

    Made a bad excuse for a worldgen

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,12 @@
+public class Block{
+    int type;
+    bool fuked;
+    bool visible;
+
+    public void setType(int t){
+        this.type = t;
+    }
+    public int getType(){
+        return this.type;
+    }
+}
--- Assets/Scripts/WorldGen.cs
+++ Assets/Scripts/WorldGen.cs
@@ -5,10 +5,36 @@ using UnityEngine;
 public class WorldGen : MonoBehaviour {
 
 	public GameObject grass_prefab;
-	// Use this for initialization
+	public GameObject dirt_prefab;
+	
+	public float blockSize;
+	public int playingfieldsize_x, playingfieldsize_y;
 	void Start () {
+
+		Block[,] bl = new Block[playingfieldsize_x, playingfieldsize_y];
+		Debug.Log(bl.Length);
+
+
+		for(int x = 0;x<playingfieldsize_x-1;x++){
+			for(int y = 0;y<playingfieldsize_y-1;y++){
+				bl[x,y] = new Block();
+				bl[x,y].setType(1);
+			}
+		}
+			//bl[0,i].setType(0);
 		
-		MonoBehaviour.Instantiate(grass_prefab,new Vector3(1,1,1),new Quaternion(0,0,0,0));
+		for(int x = 0;x<playingfieldsize_x-1;x++){
+			for(int y = 0;y<playingfieldsize_y-1;y++){
+				if(y == 23) MonoBehaviour.Instantiate(grass_prefab,new Vector3(x*blockSize,y*blockSize,1),new Quaternion(0,0,0,0));
+				else MonoBehaviour.Instantiate(dirt_prefab,new Vector3(x*blockSize,y*blockSize,1),new Quaternion(0,0,0,0));
+			}
+		}
+		
+
+
+		
+
+
 	}
 	
 	// Update is called once per frame
