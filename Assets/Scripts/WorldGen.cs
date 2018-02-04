2f4b75dfd643f0971534748640c00e2e676bb399
Theodor Dickens <d.theodor@olonaut.de>
2018-02-04 13:47:32 +0100

    Made borders of world not have holes

--- Assets/Scripts/WorldGen.cs
+++ Assets/Scripts/WorldGen.cs
@@ -11,6 +11,9 @@ public class WorldGen : MonoBehaviour {
 	public float holeChance;
 	public int playingfieldsize_x, playingfieldsize_y;
 	void Start () {
+	    // TODO REMOVE THIS BEFORE RELEASE
+		UnityEditor.SceneView.FocusWindowIfItsOpen(typeof(UnityEditor.SceneView));
+
 		// Define Block Array
 		Block[,] bl = new Block[playingfieldsize_x, playingfieldsize_y];
 		Debug.Log(bl.Length);
@@ -20,8 +23,8 @@ public class WorldGen : MonoBehaviour {
 			for(int y = 0;y<playingfieldsize_y-1;y++){
 				bl[x,y] = new Block();
 				if(y == 23) bl[x,y].setType(1);
-				else {
-					if(Random.value <= holeChance) bl[x,y].setType(0);
+				else{
+					if(Random.value <= holeChance && x != 0 && x != 23 && y != 0) bl[x,y].setType(0);
 					else bl[x,y].setType(2);
 				}
 			}
@@ -31,6 +34,7 @@ public class WorldGen : MonoBehaviour {
 		for(int x = 0;x<playingfieldsize_x-1;x++){
 			for(int y = 0;y<playingfieldsize_y-1;y++){
 				if(bl[x,y].getType() == 1) MonoBehaviour.Instantiate(grass_prefab,new Vector3(x*blockSize,y*blockSize,1),new Quaternion(0,0,0,0));
+				else if(bl[x,y].getType() == 0 ) ;
 				else MonoBehaviour.Instantiate(dirt_prefab,new Vector3(x*blockSize,y*blockSize,1),new Quaternion(0,0,0,0));
 			}
 		}
