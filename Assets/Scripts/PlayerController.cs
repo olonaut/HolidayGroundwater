b2433c16c44dd902e4121271690fbd5f991f317a
Theodor Dickens <d.theodor@olonaut.de>
2018-02-04 15:08:08 +0100

    Implemented basic player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	public float speed;
+	public float maxSpeed;
+	private Rigidbody2D r;
 	// Use this for initialization
 	void Start () {
-		
+		r = GetComponent<Rigidbody2D>();
 	}
+
+	
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float moveHorizontal = (Input.GetAxis("Horizontal") * speed);
+        r.velocity = new Vector2(moveHorizontal, r.velocity.y);
+
+		if(Input.GetButton("Jump")){
+			r.velocity = new Vector2(r.velocity.x, 15);
+		}
+
 	}
 }
