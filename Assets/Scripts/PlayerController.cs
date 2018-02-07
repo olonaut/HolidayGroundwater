0c7ab635d9e35aed194dcc1f42e6bbebe01fdd65
Theodor Dickens <d.theodor@olonaut.de>
2018-02-07 23:27:05 +0100

    Made jumping actually jump instead of fly upwards infinitely.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,21 +7,30 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	public float maxSpeed;
 	private Rigidbody2D r;
-	// Use this for initialization
+	
+	// Lock jump to prevent spam. 
+	private bool isJumpLocked = false; 
+
 	void Start () {
 		r = GetComponent<Rigidbody2D>();
 	}
 
-	
-	
 	// Update is called once per frame
 	void Update () {
         float moveHorizontal = (Input.GetAxis("Horizontal") * speed);
         r.velocity = new Vector2(moveHorizontal, r.velocity.y);
 
-		if(Input.GetButton("Jump")){
+		if(Input.GetButton("Jump") && !isJumpLocked){
 			r.velocity = new Vector2(r.velocity.x, 15);
+			isJumpLocked = true;
 		}
-
 	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		Debug.Log("I just collided with " + other.ToString() + ". Fuck!");
+		if(other.gameObject.CompareTag("JumpSurface")){
+			isJumpLocked = false;
+		}
+	}
+
 }
