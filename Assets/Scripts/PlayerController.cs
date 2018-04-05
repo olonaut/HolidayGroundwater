0cb6f81f1ef69e7fa42d354d20fbc8092ca3ecad
Theodor Dickens <d.theodor@olonaut.de>
2018-04-05 22:46:17 +0200

    I don't remember changing anything tbh

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,35 +8,42 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	public float maxSpeed;
 	private Rigidbody2D r;
+	private bool isCtrlLocked;
 	public BoxCollider2D digDownPosition, digLeftPosition, digRightPosition; // Get dig Positions from Character
 
 	public FeetController feetController;
 
 	void Start () {
 		r = GetComponent<Rigidbody2D>();
+		isCtrlLocked = false;
 	}
 
 	// Update is called once per frame
 	void Update () {
-        float moveHorizontal = (Input.GetAxis("Horizontal") * speed);
-        r.velocity = new Vector2(moveHorizontal, r.velocity.y);
 
-		if(Input.GetButton("Jump") && feetController.canJump){
-			r.velocity = new Vector2(r.velocity.x, 15);
-			feetController.canJump = false;
-		}
+		// If moving is possible
+		if(!isCtrlLocked){
+			// Multiply speed and axis tilt
+			float moveHorizontal = (Input.GetAxis("Horizontal") * speed);
+			r.velocity = new Vector2(moveHorizontal, r.velocity.y);
 
-		if(Input.GetButton("Fire1")){
-			if(Input.GetAxis("Vertical") < 0 ){
-				destoryBlockDown();
-			}
-			else if(Input.GetAxis("Horizontal") > 0){
-				destoryBlockLeft();
-			}
-			else if(Input.GetAxis("Horizontal") < 0){
-				destoryBlockRight();
+
+			if(Input.GetButton("Jump") && feetController.canJump){
+				r.velocity = new Vector2(r.velocity.x, 15);
+				feetController.canJump = false;
 			}
 			
+			if(Input.GetButton("Fire1")){
+				if(Input.GetAxis("Vertical") < 0 ){
+					destoryBlockDown();
+				}
+				else if(Input.GetAxis("Horizontal") > 0){
+					destoryBlockLeft();
+				}
+				else if(Input.GetAxis("Horizontal") < 0){
+					destoryBlockRight();
+				}	
+			}
 		}
 	}
 
